bc2840a009082c4618f8b5383041c3c0aec0d095
shahrukhsadikin <shahrukh826966@gmail.com>
2022-12-22 23:11:22 +0600

    shopcommit

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import './Prodect.css'
 const Product = (props) => {
     // console.log(props.Product)
     const {name, img, seller,wholePrice,star} = props.product;
     return (
-        <div>
+        <div className='product'>
            <img src={img} alt= "" ></img>
+           <h6>{ name}</h6>
+
         </div>
     );
 };
